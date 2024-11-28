acc34b53727567afc6576a433efad42136a4de82
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-11-28 01:37:30 -0300

    (feat) Cria as rotas principais da API.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,24 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\WalletOwnerController;
+use App\Http\Controllers\Api\V1\WalletController;
+use App\Http\Controllers\Api\V1\WalletTransferController;
+Route::prefix('v1')->group(function(){
+  
+    //Route::get('/wallets',[WalletOwnerController::class,'index']);
+    Route::apiResources([
+        'walletsowner' => WalletOwnerController::class,
 
+    ]);
+    Route::get('wallet',[WalletController::class,'index']);
+    Route::post('wallet',[WalletController::class,'store']);
+    Route::delete('wallet/{wallet}',[WalletController::class,'destroy']);
+
+    Route::post('wallet-transfer',[WalletTransferController::class,'store']);
+    Route::get('wallet-transfer',[WalletTransferController::class,'index']);
+
+});
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
