56685ca266e5031f9e79036e2a539781bbffceff
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-11-28 01:17:17 -0300

    (feat) Cria o controller do usuario.

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreUserRequest;
+class UserController extends Controller
+{
+
+  
+    public function store(StoreUserRequest $request)
+    {
+        
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
