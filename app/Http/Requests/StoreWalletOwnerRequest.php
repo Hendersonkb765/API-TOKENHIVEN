7e7b031be2f7dc5a290080378f95ab8a89452e07
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-11-28 01:24:22 -0300

    (feat) Cria validação do controller Wallet do método store.

---
+++ app/Http/Requests/StoreWalletOwnerRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use App\Traits\HttpResponses;
+
+class StoreWalletOwnerRequest extends FormRequest
+{
+    use HttpResponses;
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string',
+            'email' => 'required|email|unique:wallet_owners,email',
+        ];
+    }
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = $validator->errors();
+
+        throw new HttpResponseException($this->error('Validation error', 422, $errors));
+    }
+}
