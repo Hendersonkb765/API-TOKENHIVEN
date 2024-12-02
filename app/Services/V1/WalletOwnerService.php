f3ad1f54bc9f711e482b0083e6492f3d323e11d9
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-12-02 20:17:56 -0300

    (feat) Cria  o service walletOwnerService.

---
+++ app/Services/V1/WalletOwnerService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Services\V1;
+
+use App\Models\Wallet;
+use App\Services\V1\TokenUserResolverService;
+use App\Models\WalletOwner;
+use Illuminate\Http\Request;
+use Illuminate\Database\Eloquent\Model;
+class WalletOwnerService{
+
+    public function createWalletOwner(Request $request):Model{
+       
+        $user = (new TokenUserResolverService())->getUser($request);
+        $validated = $request->validated();
+        $walletOwner = WalletOwner::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'system_manager_id'=> $user->id
+        ]);
+         
+
+        return $walletOwner;
+    }
+}
+
+?>
