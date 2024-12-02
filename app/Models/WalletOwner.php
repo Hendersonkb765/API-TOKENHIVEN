30eabf277e75ded5ebc592185e7633ec4a9acb6e
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-12-02 20:08:47 -0300

    (feat) Adiciona o método scopeAllFiltered no model WalletOwner.

--- app/Models/WalletOwner.php
+++ app/Models/WalletOwner.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Wallet;
+use Illuminate\Http\Request;
+use App\Services\V1\TokenUserResolverService;
 class WalletOwner extends Model
 {
     /** @use HasFactory<\Database\Factories\WalletOwnerFactory> */
@@ -15,6 +17,11 @@ class WalletOwner extends Model
     public function wallet()
     {
         return $this->hasOne(Wallet::class, 'owner_id');
+    }
+
+    public function scopeAllFiltered($query, Request $request){
+        $user  =(new TokenUserResolverService())->getUser($request);
+        return $user;
     }
    
 }
