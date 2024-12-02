42bd1321d5b00ab89c686b09c1dd8c265d070ba4
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-12-02 19:59:26 -0300

    (feat) Cria a página de layout.

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-  @vite(['resources/js/app.js','resources/css/style.css'])
+  @vite(['resources/css/login.css','resources/js/app.js','resources/css/style.css',])
   <title>Document</title>
 </head>
 <body>
@@ -47,7 +47,7 @@
               <a class="nav-link" href="#">Contato</a>
             </li>
             <li class="nav-item ">
-              <a class="btn-login" href="{{route('users.create')}}">Login</a>
+              <a class="btn-login">Login</a>
             </li>
           </ul>
           <form class="d-flex mt-3" role="search">
