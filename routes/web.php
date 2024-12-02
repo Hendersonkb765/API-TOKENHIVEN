e6cbe15b344a7c5e349f9bc72e4981cd711f7343
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-12-02 20:20:48 -0300

    (feat) Adiciona rotas para login, registro e gerenciamento de tokens de acesso

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,17 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\AccessTokenController;
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/login',function(){
+    return view('login');
+});
+Route::get('/register',[UserController::class,'create'])->name('users.create');
 
-Route::post('users/create',[UserController::class,'store']);
+Route::post('/users',[UserController::class,'store'])->name('users.store');
+Route::post('/access-tokens',[AccessTokenController::class,'store'])->name('accessTokens.store');
+Route::get('/access-tokens',[AccessTokenController::class,'index'])->name('accessTokens.index');
+
+
