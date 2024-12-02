d3116d0e63e377df699fb49255062b607dcc7693
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-12-02 20:14:13 -0300

    (feat) Cria o service TokenUserResolverService, com o método getUser(), responsavel por retornar o token do usuário.

---
+++ app/Services/V1/TokenUserResolverService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Services\V1;
+use Illuminate\Http\Request;
+use Laravel\Sanctum\PersonalAccessToken;
+use Illuminate\Support\Str;
+use App\Models\User;
+use App\Exceptions\TokenNotFoundException;
+class TokenUserResolverService{
+
+    public function getUser(Request $request): User{
+        
+        $authorizationHeader = $request->header('Authorization');
+        $token = PersonalAccessToken::findToken(Str::after($authorizationHeader  , 'Bearer '));
+        $user = $token->tokenable;
+        if(!$user){
+            throw new TokenNotFoundException();
+        }
+        return $user;
+    }
+}
+
+
