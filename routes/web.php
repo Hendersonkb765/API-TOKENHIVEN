a6c5a3ea6ee59fde863d68f5fe2375ee4e541461
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-12-11 16:52:41 -0300

    comenta as rotas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AccessTokenController;
+/*
 Route::get('/', function () {
     return view('welcome');
 });
@@ -13,5 +14,5 @@ Route::get('/register',[UserController::class,'create'])->name('users.create');
 Route::post('/users',[UserController::class,'store'])->name('users.store');
 Route::post('/access-tokens',[AccessTokenController::class,'store'])->name('accessTokens.store');
 Route::get('/access-tokens',[AccessTokenController::class,'index'])->name('accessTokens.index');
-
+*/
 
