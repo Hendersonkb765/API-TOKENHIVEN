dd5cf5eaeb8e3a7f848a4568f069167364a7d9d4
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-12-11 15:15:29 -0300

    (feat) Adiciona as rotas do walletTransfer

--- routes/api.php
+++ routes/api.php
@@ -9,13 +9,11 @@ Route::prefix('v1')->group(function(){
   
     //Route::get('/wallets',[WalletOwnerController::class,'index']);
     Route::apiResource('walletsowner', WalletOwnerController::class)->middleware('auth:sanctum');
-    Route::get('wallet',[WalletController::class,'index'])->middleware('auth:sanctum');
-    Route::post('wallet',[WalletController::class,'store']);
-    Route::delete('wallet/{wallet}',[WalletController::class,'destroy']);
 
-    Route::post('wallet-transfer',[WalletTransferController::class,'store']);
-    Route::get('wallet-transfer',[WalletTransferController::class,'index']);
-
+    Route::apiResource('wallets', WalletController::class)->middleware('auth:sanctum');
+  
+    Route::post('wallet-transfer',[WalletTransferController::class,'store'])->middleware('auth:sanctum')->name('historicTransfer.store');
+    Route::get('wallet-transfer',[WalletTransferController::class,'index'])->middleware('auth:sanctum')->name('historicTransfer.index');
 });
 Route::get('/user', function (Request $request) {
     return $request->user();
