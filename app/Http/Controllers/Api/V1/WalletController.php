81e7132f0283795daa5d7c9091b9f37ba1c11cd5
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-12-11 14:45:43 -0300

    (feat) Adiciona o filtro wallet ao controller WalletController.

--- app/Http/Controllers/Api/V1/WalletController.php
+++ app/Http/Controllers/Api/V1/WalletController.php
@@ -11,15 +11,23 @@ use App\Http\Resources\V1\WalletResource;
 use Illuminate\Database\QueryException;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
+use App\Filters\WalletFilter;
+use App\Services\V1\TokenUserResolverService;
+use App\Repositories\BaseRepository;
+
 class WalletController extends Controller
 {
     use HttpResponses;
-    public function index()
+
+
+    public function index(Request $request)
     {
         try{
-
-
-            return $this->success('ok',200,WalletResource::collection(Wallet::all()));
+            $queryFilter = (new WalletFilter())->filter($request);
+            $userId = (new TokenUserResolverService())->getUser($request)->id;
+            $wallets = (new BaseRepository(new Wallet(),$userId))->all($queryFilter);
+            
+            return $this->success('ok',200,WalletResource::collection($wallets));
             
         }
         catch(\QueryException $e){
@@ -31,7 +39,7 @@ class WalletController extends Controller
         catch(\Exception $e){
             Log::error('Error:'.$e->getMessage());
 
-            return $this->error('An unexpected error occurred. Please contact support if the problem persists.',500);
+            return $this->error($e->getMessage(),500);
         }
         
     }
@@ -40,14 +48,17 @@ class WalletController extends Controller
     public function store(StoreWalletRequest $request)
     {
         try{
-            $request = $request->validated();
-            $wallet = Wallet::create([
+            $validated = $request->validated();
+            $userId = (new TokenUserResolverService())->getUser($request)->id;
+            if(Wallet::where('owner_id',$request['owner_id'])->exists()){
+                return $this->error('There is already a linked wallet',400);
+            }
+            $wallet = (new BaseRepository(new Wallet,$userId))->create([
                 'wallet_address'=>Hash::make(now()),
                 'amount'=>0,
-                'owner_id'=>$request['owner_id'],
-                'system_manager_id'=>1,
-
+                'owner_id'=>$request['ownerId'],
             ]);
+           
             return $this->success('Wallet created successfully',201,new WalletResource($wallet));
             
         }
@@ -57,15 +68,34 @@ class WalletController extends Controller
         }
         catch(\Exception $e){
             Log::error('Error:'.$e->getMessage());
-            return $this->error('An unexpected error occurred. Please contact support if the problem persists.',500);
+            return $this->error($e->getMessage(),500);
         }
             
     }
-
-
-    public function destroy(Wallet $wallet)
+    public function show(Request $request ,Wallet $wallet)
     {
         try{
+            $userId = (new TokenUserResolverService())->getUser($request)->id;
+            $wallet = (new BaseRepository($wallet,$userId))->show($wallet);
+            return $this->success('ok',200,new WalletResource($wallet));
+        }
+        catch(\QueryException $e){
+            Log::error('Error QueryException'.$e->getMessage());
+            return $this->error('An error occurred while trying to get the wallet',500);
+        }
+        catch(\Exception $e){
+            Log::error('Error:'.$e->getMessage());
+            return $this->error($e->getMessage(),500);
+        }
+        
+    }
+
+
+    public function destroy(Request $request,Wallet $wallet)
+    {
+        try{
+            $userId = (new TokenUserResolverService())->getUser($request)->id;
+            (new BaseRepository($wallet,$userId))->delete($wallet);
             $wallet->delete();
             return $this->success('Wallet deleted successfully',200);
         }
@@ -75,7 +105,7 @@ class WalletController extends Controller
         }
         catch(\Exception $e){
             Log::error('Error:'.$e->getMessage());
-            return $this->error('An unexpected error occurred. Please contact support if the problem persists.',500);
+            return $this->error($e->getMessage(),500);
         }
         
     }
