866e6cbc4b8b96df72f6d5e22504cf021f6c718a
hendersonkb765 <hendersonkb765@outlook.com.br>
2024-12-11 15:16:12 -0300

    (feat) Cria o teste WalletOwnerTest.

--- tests/Feature/WalletOwnerTest.php
+++ tests/Feature/WalletOwnerTest.php
@@ -2,17 +2,26 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use App\Models\WalletOwner;
 use Tests\TestCase;
+use App\Models\User;
+
 
 class WalletOwnerTest extends TestCase
 {
+
     /**
      * A basic feature test example.
      */
-    protected $token = '12|fjztor2uhOmHzLP56HaDfiRWNbja5ZTbNRl7A0mV99da71dc';
+    protected $token;
+    protected $userId;
+    public function setUp():void{
+        parent::setUp();
+        $user = User::factory()->create();
+        $this->token = $user->createToken('my-app-token')->plainTextToken;
+        $this->userId = $user->id;
+    }
     public function test_create_wallet_owner(): void
     { 
         $emailRandom= rand(1, 1000000);
@@ -29,9 +38,13 @@ class WalletOwnerTest extends TestCase
             'status',
             'data'
         ]);
+
     }
     public function test_get_all_wallet_owner(): void
     {
+        WalletOwner::factory(20)->create([
+            'system_manager_id'=> $this->userId
+        ]);
         $response = $this->withHeaders([
             'Authorization'=> "Bearer $this->token"
             ])
@@ -47,10 +60,14 @@ class WalletOwnerTest extends TestCase
     }
     public function test_get_wallet_owner(): void
     {
+        $created = WalletOwner::factory(1)->create([
+            'system_manager_id'=> $this->userId
+        ]);
+
         $response = $this->withHeaders([
             'Authorization'=> "Bearer $this->token"
             ])
-        ->get('api/v1/walletsowner/2');
+        ->get('api/v1/walletsowner/'.$created->first()->id);
 
         $response->assertStatus(200);
         $response->assertJsonStructure([
@@ -61,10 +78,13 @@ class WalletOwnerTest extends TestCase
         
     }
     public function test_update_wallet_owner_data():void{
+
+        $created = WalletOwner::factory()->create(['system_manager_id'=>$this->userId]);
         $response = $this->withHeaders([
             'Authorization' =>"Bearer $this->token"
-        ])->put('api/v1/walletsowner/2',[
-            "name"=> "Henderson gomes",
+        ])->put('api/v1/walletsowner/'.$created->id,[
+            "name"=> "Henderson gomes"
+
         ]);
 
         $response->assertStatus(200);
@@ -78,8 +98,8 @@ class WalletOwnerTest extends TestCase
 
         $created = WalletOwner::create([
             'name' => 'Henderson gomes',
-            'email' => 'tess@test.com',
-            'system_manager_id'=> 1]);
+            'email' => 'tesss@test.com',
+            'system_manager_id'=> $this->userId]);
 
         $response = $this->withHeaders([
             'Authorization' => "Bearer $this->token"])
